252360bac66428dcdf8cf5faedbfdef2b4b52745
Rithvik <rithvik81107@gmail.com>
2026-08-30 16:29:50 +0530

    Add counter and ALU RTL projects

---
+++ 01_counter/counter.v
@@ -0,0 +1,15 @@
+module counter(
+    input clk,
+    input reset,
+    output reg[2:0] count
+);
+
+always @(posedge clk)
+begin
+    if(reset)
+        count <= 0;
+    else
+        count <= count+1;
+end
+
+endmodule
